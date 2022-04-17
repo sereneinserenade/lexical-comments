3f4073779742530e1baf99ef9bf4f6efe0ce05b2
Jeet Mandaliya <45892659+sereneinserenade@users.noreply.github.com>
2022-04-17 15:41:41 +0200

    github actions for deploying to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/lexical-comments/' : '/'
+  }
 })
